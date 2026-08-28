fbb625b5a64dc0c3dc0c9369f686051c1ee619ed
acculief <acculief@users.noreply.github.com>
2026-08-28 10:21:39 +0900

    content: 年収550万円の手取りはいくら？内訳と早見表

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -12,6 +12,7 @@ const routes = [
   { path: "/nennshu-350-tedori/", priority: 0.8 },
   { path: "/nennshu-400-tedori/", priority: 0.8 },
   { path: "/nennshu-500-tedori/", priority: 0.8 },
+  { path: "/nennshu-550-tedori/", priority: 0.8 },
   { path: "/nennshu-600-tedori/", priority: 0.8 },
   { path: "/nennshu-700-tedori/", priority: 0.8 },
   { path: "/nennshu-800-tedori/", priority: 0.8 },
